43544d9871e3e820c0931d8c4129ee701c90ff55
Michael Dedun <michael.dedun@mail.ru>
2020-05-06 18:16:17 +0300

    [#270180]

--- chapter_001/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_001/src/main/java/ru/job4j/iterator/Converter.java
@@ -7,7 +7,7 @@ class Converter {
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
         return new Iterator<>() {
             private Iterator<Iterator<Integer>> external = it;
-            private Iterator<Integer> current = it.next();
+            private Iterator<Integer> current = it == null ? Collections.emptyIterator() : it.next();
 
             @Override
             public boolean hasNext() {
