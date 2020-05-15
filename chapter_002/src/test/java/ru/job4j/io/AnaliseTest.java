47b4f7bafd9e33fc15dd038c28a8ea96edc9ba82
Michael Dedun <michael.dedun@mail.ru>
2020-05-15 20:49:48 +0300

    Задание [#270208]

--- chapter_002/src/test/java/ru/job4j/io/AnaliseTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnaliseTest.java
@@ -1,14 +1,17 @@
 package ru.job4j.io;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.stream.Collectors;
 
 public class AnaliseTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void checkUnavailable() throws IOException {
@@ -22,6 +25,25 @@ public class AnaliseTest {
             break;
         }
         in.close();
+    }
+
+    @Test
+    public void checkUnavailableWithTemporary() throws IOException {
+        Analise analise = new Analise();
+        File source = folder.newFile("source");
+        File result = folder.newFile("target");
+        StringBuilder rsl = new StringBuilder();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+        }
+        analise.unavailable(source.getAbsolutePath(), result.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(result))) {
+            in.lines().forEach(rsl::append);
+        }
+        Assert.assertEquals("10:57:01 до 10:59:01", rsl.toString());
     }
 
 }
