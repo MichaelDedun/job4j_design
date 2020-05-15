b0f5965fb77bc16ab72e782a09ff726f517fddad
Michael Dedun <michael.dedun@mail.ru>
2020-05-15 23:41:05 +0300

    Задание [#270209]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -3,14 +3,15 @@ package ru.job4j.io;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        search(Path.of(args[0]), args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
