2508f06c863442a65555896fbd059ad185c3d2c0
Michael Dedun <michael.dedun@mail.ru>
2020-05-15 18:50:53 +0300

    Задание [#270210]

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    out.write(("\n" + (i * j)).getBytes());
+                    out.write((System.lineSeparator() + (i * j)).getBytes());
                 }
             }
         } catch (Exception e) {
