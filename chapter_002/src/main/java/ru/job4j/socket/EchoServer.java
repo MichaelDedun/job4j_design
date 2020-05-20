bc6eec3d16022010189fb52c9954229689c5b433
Michael Dedun <michael.dedun@mail.ru>
2020-05-20 18:28:32 +0300

    [4850#270224]

---
+++ chapter_002/src/main/java/ru/job4j/socket/EchoServer.java
@@ -0,0 +1,41 @@
+package ru.job4j.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (true) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                        if (str.contains("Bye")) {
+                            try {
+                                server.close();
+                                System.out.println("server has been stopped");
+                                out.write("server stopped\r\n\\".getBytes());
+                            } catch (SocketException ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    }
+                    if (!server.isClosed()) {
+                        out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    }
+                }
+            }
+        }
+    }
+
+}
