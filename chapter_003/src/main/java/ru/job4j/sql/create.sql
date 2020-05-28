dee5c4926bf1b7741c0ffdbe97732a8e274e677d
Michael Dedun <michael.dedun@mail.ru>
2020-05-28 16:12:02 +0300

    Задание [1093#270152]

--- chapter_003/src/main/java/ru/job4j/sql/create.sql
+++ chapter_003/src/main/java/ru/job4j/sql/create.sql
@@ -5,19 +5,18 @@ CREATE DATABASE db_for_task (
     CONNECTION LIMIT = -1;
 );
 
+CREATE TABLE "role" (
+	id SERIAL PRIMARY KEY,
+	role VARCHAR(20),
+);
+
 CREATE TABLE "user" (
 	id SERIAL PRIMARY KEY,
 	login VARCHAR(20),
 	password VARCHAR(20),
 	first_name VARCHAR(255),
-	last_name VARCHAR(255)
-);
-
-CREATE TABLE "role" (
-	id SERIAL PRIMARY KEY,
-	role_type VARCHAR(20),
-	user_id INT4,
-	FOREIGN KEY (user_id) REFERENCES "user" (id)
+	last_name VARCHAR(255),
+	role_id INT4 REFERENCES "role"(id)
 );
 
 CREATE TABLE "rules" (
@@ -28,13 +27,26 @@ CREATE TABLE "rules" (
 CREATE TABLE "user_role_rules" (
 	id SERIAL PRIMARY KEY,
 	role_id INT4 REFERENCES "role"(id),
-	rule_id INT4 REFERENCES "rules"
+	rule_id INT4 REFERENCES "rules"(id)
+);
+
+CREATE TABLE "category" (
+	id SERIAL PRIMARY KEY,
+	category_name VARCHAR(30),
+);
+
+CREATE TABLE "state" (
+	id SERIAL PRIMARY KEY,
+	state VARCHAR(30)
 );
 
 CREATE TABLE "items" (
 	id SERIAL PRIMARY KEY,
 	description VARCHAR(255),
-	user_id INT4 REFERENCES "user"(id)
+	user_id INT4 REFERENCES "user"(id),
+	category_id INT4 REFERENCES "category"(id),
+	state_id INT4 REFERENCES "state"(id)
+
 );
 
 CREATE TABLE "comments" (
@@ -49,36 +61,28 @@ CREATE TABLE "attachs" (
 	item_id INT4 REFERENCES "items"(id)
 );
 
-CREATE TABLE "category" (
-	id SERIAL PRIMARY KEY,
-	category_name VARCHAR(30),
-	item_id INT4 REFERENCES "items"(id)
-);
+INSERT INTO "role" (role) VALUES ('admin');
+INSERT INTO "role" (role) VALUES ('main_admin');
 
-CREATE TABLE "state" (
-	id SERIAL PRIMARY KEY,
-	state VARCHAR(30),
-	item_id INT4 REFERENCES "items"(id)
-);
+INSERT INTO "user" (login, password, first_name, last_name, role_id) VALUES ('ms_hant2011', '123321qq', 'Misha', 'Dedun', 1);
 
-INSERT INTO "user" (login, password, first_name, last_name) VALUES ('ms_hant2011', '123321qq', 'Misha', 'Dedun');
 
-INSERT INTO "role" (role_type, user_id) VALUES ('admin', '1');
-INSERT INTO "role" (role_type, user_id) VALUES ('main_admin', '1');
 
 INSERT INTO "rules" (rule) VALUES ('delete user');
 
-INSERT INTO "user_role_rules" (role_id, rule_id) VALUES (1,1);
+INSERT INTO "user_role_rules" (role_id, rule_id) VALUES (1, 1);
 
-INSERT INTO "items" (description, user_id) VALUES ('Сумка',1);
+INSERT INTO "category" (category_name) VALUES ('Спорт');
 
-INSERT INTO "comments" (description, item_id) VALUES ('сумка хоккейная',1);
+INSERT INTO "state" (state) VALUES ('В ожидании');
 
-INSERT INTO "attachs" (file_name, item_id) VALUES ('word.txt',1);
+INSERT INTO "items" (description, user_id, category_id, state_id) VALUES ('Сумка',1 ,1 ,1);
 
-INSERT INTO "category" (category_name, item_id) VALUES ('Спорт',1);
+INSERT INTO "comments" (description, item_id) VALUES ('сумка хоккейная', 1);
 
-INSERT INTO "state" (state, item_id) VALUES ('В ожидании',1);
+INSERT INTO "attachs" (file_name, item_id) VALUES ('word.txt', 1);
+
+
 
 
 
