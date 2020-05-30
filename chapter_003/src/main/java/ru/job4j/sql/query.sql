eaa34059618bc36188efd3b374f6ccf705372957
Michael Dedun <michael.dedun@mail.ru>
2020-05-30 16:25:47 +0300

    Задание [1732#270155]

--- chapter_003/src/main/java/ru/job4j/sql/query.sql
+++ chapter_003/src/main/java/ru/job4j/sql/query.sql
@@ -26,6 +26,8 @@ INSERT INTO product (name, type_id, expired_date, price)
 VALUES ('сметана', 2, '2020-07-08', 200);
 INSERT INTO product (name, type_id, expired_date, price)
 VALUES ('маскарпоне', 1, '2020-08-08', 400);
+INSERT INTO product (name, type_id, expired_date, price)
+VALUES ('маскарпоне', 1, '2020-08-08', 400);
 
 
 SELECT *
@@ -37,31 +39,44 @@ SELECT *
 FROM product
 WHERE name LIKE '%мороженное%';
 
+-- SELECT *
+-- FROM product
+-- WHERE EXTRACT(MONTH FROM expired_date) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP) + 1;
+
 SELECT *
 FROM product
-WHERE EXTRACT(MONTH FROM expired_date) = EXTRACT(MONTH FROM CURRENT_TIMESTAMP) + 1;
+WHERE EXTRACT(MONTH FROM expired_date) <= EXTRACT(MONTH FROM current_date + interval '1 month');
 
-SELECT MAX(price)
-FROM product;
+SELECT name, price
+from product
+order by price desc
+limit 1;
 
-SELECT COUNT(type.name)
+SELECT type.name, COUNT(type.name)
 from type
-         inner join product p on type.id = p.type_id;
+         join product p on type.id = p.type_id
+group by type.name;
 
 SELECT t.name, COUNT(type_id)
 FROM product
          inner join type t on product.type_id = t.id
 group by t.name;
 
-SELECT name
-FROM (SELECT t.name, COUNT(t.id) as count
-      FROM product
-               inner join type t on product.type_id = t.id
-      group by t.name)
-         as nc
-WHERE nc.count < 10;
+SELECT product.name
+FROM product
+         JOIN type t on product.type_id = t.id
+where t.name = 'СЫР'
+   or t.name = 'МОЛОКО';
+
+select type.name
+from type
+         join product p on type.id = p.type_id
+group by type.name
+HAVING COUNT(*) < 10;
 
 SELECT t.name, p.name
 FROM product AS p
-         join type t on p.type_id = t.id
+         join type t on p.type_id = t.id;
+
+
 
