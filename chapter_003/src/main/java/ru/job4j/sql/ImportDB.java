5fbb58c1f3b75f649d06c8a31822fc73cccf39aa
Michael Dedun <michael.dedun@mail.ru>
2020-06-02 19:06:04 +0300

    Задание [20459#270137]

---
+++ chapter_003/src/main/java/ru/job4j/sql/ImportDB.java
@@ -0,0 +1,66 @@
+package ru.job4j.sql;
+
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            rd.lines().forEach(s -> {
+                String[] data = s.split(";");
+                users.add(new User(data[0], data[1]));
+            });
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws SQLException {
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement("INSERT INTO users (name, email) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS)) {
+                    ps.setString(1, user.name);
+                    ps.setString(2, user.email);
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        String name;
+        String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("./chapter_003/src/main/resources/app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "./dump.txt");
+        db.save(db.load());
+    }
+
+}
---
+++ chapter_003/src/main/java/ru/job4j/sql/user.sql
@@ -0,0 +1,5 @@
+create table users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
+	email VARCHAR(30) NOT NULL
+);
