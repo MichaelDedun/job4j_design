d73c5d696250515e3abd28c7ccdef48e83bf5c12
Michael Dedun <michael.dedun@mail.ru>
2020-07-27 14:25:51 +0300

    Задание [229175#317845]

---
+++ chapter_006/src/main/java/ru/job4j/ThreadState.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getState());
+        first.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
+        System.out.println(first.getState());
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(second.getState());
+        second.start();
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getState());
+        }
+        System.out.println(second.getState());
+        System.out.println("Работа завершена");
+    }
+
+}
